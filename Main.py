87df0344b2c006b2a91751d7a5df2fabeff5b965
ikromjonpchilqoziyev-crypto <ikromjonpchilqoziyev@gmail.com>
2026-02-20 16:24:06 +0500

    Add transport classes with movement methods
    1

--- Main.py
+++ Main.py
@@ -1,1 +1,24 @@
-main
+class Transport:
+    def harakatlanish(self):
+        print("Transport harakatlanmoqda")
+
+class Avtomobil(Transport):
+    def harakatlanish(self):
+        print("Avtomobil 80 km/soat tezlikda harakatlanmoqda")
+
+class Velosiped(Transport):
+    def harakatlanish(self):
+        print("Velosiped 20 km/soat tezlikda harakatlanmoqda")
+
+class Poyezd(Transport):
+    def harakatlanish(self):
+        print("Poyezd 120 km/soat tezlikda harakatlanmoqda")
+
+transportlar = [
+    Avtomobil(),
+    Velosiped(),
+    Poyezd()
+]
+
+for t in transportlar:
+    t.harakatlanish()
